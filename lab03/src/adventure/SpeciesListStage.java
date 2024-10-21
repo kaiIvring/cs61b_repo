c3f448d847d1a6bdc3fa36b01b8d25ff5f9f25c5
KaiIvring <3011973335@qq.com>
2024-10-21 21:53:18 +0800

    Adventure one more to go!

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -76,11 +76,13 @@ public class SpeciesListStage implements AdventureStage {
             } else {
                 user = Arrays.asList(input.toLowerCase().split(" *, *"));
             }
-            double similarity = arraySimilarity(reference, user);
-            if (similarity != 1 && reference.size() != 0) {
-                long numCorrect = Math.round(similarity * reference.size());
-                System.out.println("Try again! You got " + numCorrect + " animals correct!");
-                continue;
+            if (reference.size() != 0) {
+                double similarity = arraySimilarity(reference, user);
+                if (similarity != 1 && reference.size() != 0) {
+                    long numCorrect = Math.round(similarity * reference.size());
+                    System.out.println("Try again! You got " + numCorrect + " animals correct!");
+                    continue;
+                }
             }
             break;
         }
