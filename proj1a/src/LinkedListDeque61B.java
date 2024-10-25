e42be424c662933cfb6a84da44f083f8f45e3889
KaiIvring <3011973335@qq.com>
2024-10-25 08:23:15 +0800

    get started!

---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+
+    public LinkedListDeque61B() {
+
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
