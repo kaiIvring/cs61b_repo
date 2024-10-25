4ea97e512712fa6d3cd4693ea23310ebfab78275
KaiIvring <3011973335@qq.com>
2024-10-25 09:15:08 +0800

    addLast addFirst

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -2,18 +2,42 @@ import java.util.List;
 
 public class LinkedListDeque61B<T> implements Deque61B<T> {
 
-    public LinkedListDeque61B() {
+    private Node sentinel;
+    private int size;
 
+    private class Node {
+        public T item;
+        public Node next;
+        public Node prev;
+
+        public Node (T i,Node n,Node p) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    public LinkedListDeque61B() {
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
     }
 
     @Override
     public void addFirst(T x) {
-
+        Node newNode = new Node(x,sentinel.next,sentinel);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
     }
 
     @Override
     public void addLast(T x) {
-
+        Node newNode = new Node(x,sentinel,sentinel.prev);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size += 1;
     }
 
     @Override
