2a0c4d3b0f19ae3b8cf0e1848f26ccf667d6ed16
KaiIvring <3011973335@qq.com>
2024-11-14 10:54:18 +0800

    proj1c finished! (finally!)

--- proj1c/src/gh2/GuitarString.java
+++ proj1c/src/gh2/GuitarString.java
@@ -1,7 +1,5 @@
 package gh2;
 
-// TODO: maybe more imports
-
 import deque.Deque61B;
 import deque.LinkedListDeque61B;
 
@@ -14,15 +12,10 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
     private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Initialize the buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer with zeros.
         buffer = new LinkedListDeque61B<>();
         int capacity = (int) Math.round(SR / frequency);
         for (int i = 0; i < capacity; i++) {
@@ -30,13 +23,8 @@ public class GuitarString {
         }
     }
 
-
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
@@ -50,20 +38,23 @@ public class GuitarString {
         }
     }
 
-
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        if (buffer.size() < 2) {
+            throw new IllegalStateException("Buffer must have as least two item to apply tic");
+        }
+
+        double firstItem = buffer.removeFirst();
+        double secondItem = buffer.get(0);
+        double average = (firstItem + secondItem) / 2;
+        double newItem = average * DECAY;
+        buffer.addLast(newItem);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
