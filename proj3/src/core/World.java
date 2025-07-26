1e2025fbbc113e2ae1b610812c37b81da703ae3d
KaiIvring <3011973335@qq.com>
2025-07-26 12:01:49 +0800

    start working on the project
    add a few instance variables
    trying to figure out the world generation algorithm

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,6 +1,34 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.Random;
+
 public class World {
+    private static final int DEFAULT_WIDTH = 50;
+    private static final int DEFAULT_HEIGHT = 50;
+    private TERenderer ter;
+    private Random random;
+
+    public World() {
+
+    }
+
+    /**
+     * Fills the 2D array of tiles with NOTHING tiles.
+     * @param tiles
+     */
+    public void fillWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
 
     // build your own world!
 
